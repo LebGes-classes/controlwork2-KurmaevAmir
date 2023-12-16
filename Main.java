aa660235f1bee7e8999f4f243fbcf457eca0ec09
AmirKurmaev <amirku05@yandex.ru>
2023-12-16 14:10:36 +0300

    Fix

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main { // Variant 3
     public static void main(String[] args) {
-        int numberStudents = 10;
-        Student[] queue = new Student[10];
+        int numberStudents = 5;
+        Student[] queue = new Student[numberStudents];
         for (int i = 0; i < numberStudents; i++) {
             if (i % 2 == 0) {
                 queue[i] = new ITIS();
@@ -21,7 +21,7 @@ public class Main { // Variant 3
                 if (queue[i].getPoints() > queue[i + 1].getPoints()) {
                     temp = queue[i + 1];
                     queue[i + 1] = queue[i];
-                    queue[i] = queue[i + 1];
+                    queue[i] = temp;
                 }
             }
         }
